21c99b9f9e62863221a2c457caf230b96eb9d6b6
Samuel Bak <samuelbak@samyuerunoiMac.local>
2020-07-29 13:06:36 +0900

    2

---
+++ don2.js
@@ -0,0 +1,63 @@
+
+new gridjs.Grid({
+  columns: [{
+    name: "平均出玉",
+    width: "auto",
+  }, {
+    name: "純増",
+    width: "auto",
+  }, {
+    name: "回転数",
+    width: "auto",
+  }, {
+    name: "破壊持続",
+    width: "auto"
+  }],
+  search: false,
+  data: [
+    ["検証中", "検証中", "検証中", "1.0倍"]
+  ],
+  style: {
+    th: {
+      "padding": "1.5%",
+      "text-align": "center",
+      "font-size": "100%",
+    },
+    td: {
+      "text-align": "center",
+      "padding": "1%",
+    }
+  }
+}).render(document.getElementById("wrapper"));
+
+
+new gridjs.Grid({
+  columns: [{
+    name: "平均出玉",
+    width: "auto",
+  }, {
+    name: "純増",
+    width: "auto",
+  }, {
+    name: "回転数",
+    width: "auto",
+  }, {
+    name: "破壊持続",
+    width: "auto"
+  }],
+  search: false,
+  data: [
+    ["時速 約22,269枚", "約19.8枚", "時速 約1,125回転", "1.0倍"]
+  ],
+  style: {
+    th: {
+      "padding": "1.5%",
+      "text-align": "center",
+      "font-size": "100%",
+    },
+    td: {
+      "text-align": "center",
+      "padding": "1%",
+    }
+  }
+}).render(document.getElementById("emperor"));
---
+++ tekken4.js
@@ -0,0 +1,63 @@
+
+new gridjs.Grid({
+  columns: [{
+    name: "平均出玉",
+    width: "auto",
+  }, {
+    name: "純増",
+    width: "auto",
+  }, {
+    name: "回転数",
+    width: "auto",
+  }, {
+    name: "破壊持続",
+    width: "auto"
+  }],
+  search: false,
+  data: [
+    ["検証中", "検証中", "検証中", "1.0倍"]
+  ],
+  style: {
+    th: {
+      "padding": "1.5%",
+      "text-align": "center",
+      "font-size": "100%",
+    },
+    td: {
+      "text-align": "center",
+      "padding": "1%",
+    }
+  }
+}).render(document.getElementById("wrapper"));
+
+
+new gridjs.Grid({
+  columns: [{
+    name: "平均出玉",
+    width: "auto",
+  }, {
+    name: "純増",
+    width: "auto",
+  }, {
+    name: "回転数",
+    width: "auto",
+  }, {
+    name: "破壊持続",
+    width: "auto"
+  }],
+  search: false,
+  data: [
+    ["時速 約14,184枚", "約2.9枚", "時速 約4,950回転", "1.0倍"]
+  ],
+  style: {
+    th: {
+      "padding": "1.5%",
+      "text-align": "center",
+      "font-size": "100%",
+    },
+    td: {
+      "text-align": "center",
+      "padding": "1%",
+    }
+  }
+}).render(document.getElementById("emperor"));
